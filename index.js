b1aa9a13b0b0a04b9e2513eb2e27c7164c6bc92d
Ben Gourley <bn@grly.me>
2021-01-14 09:43:40 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import Bugsnag from "@bugsnag/react-native";
+Bugsnag.start();
+
+Bugsnag.notify('hi')
+
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
